e9acf4a46f09b177dfa1cc0613e1b1042fc6482c
AdamB25 <barsialtunit@gmail.com>
2024-11-26 17:18:12 -0500

    CukesRunner done

---
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -0,0 +1,24 @@
+package com.cydeo.runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"html:target/default.html",
+"me.jvt.cucumber.report.PrettyReports:target/cucumber"
+        },
+        features = {"src/test/resources/feature/api","src/test/resources/feature/ui"},
+        glue = "com/cydeo/step_definitions",
+        dryRun = true,
+        tags = "@login",
+        publish = true
+
+)
+
+
+
+public class CukesRunner {
+}
