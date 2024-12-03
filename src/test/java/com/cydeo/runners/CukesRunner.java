66739377cea1c3e3df901c34ed22ed50655526fd
AdamB25 <barsialtunit@gmail.com>
2024-12-03 16:53:26 -0500

    BrowserUtils started

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -8,16 +8,15 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {"html:target/default.html",
-"me.jvt.cucumber.report.PrettyReports:target/cucumber"
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
         },
-        features = {"src/test/resources/feature/api","src/test/resources/feature/ui"},
+        features = {"src/test/resources/feature/api", "src/test/resources/feature/ui"},
         glue = "com/cydeo/step_definitions",
         dryRun = true,
         tags = "@login",
         publish = true
 
 )
-
 
 
 public class CukesRunner {
---
+++ src/test/java/com/cydeo/utilities/BrowserUtils.java
@@ -0,0 +1,62 @@
+package com.cydeo.utilities;
+
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class BrowserUtils {
+
+    //static wait
+
+    public static void wait(int seconds) {
+        try {
+            Thread.sleep(seconds * 1000);
+        } catch (InterruptedException e) {
+
+        }
+    }
+
+
+    //I will get the element as a parameter and I will hover over it
+
+    public static void hoverElement(WebElement element) {
+        Actions actions = new Actions(Driver.get());
+        actions.moveToElement(element).perform();
+    }
+
+    //I will get the title as a parameter and I will verify the title
+
+    public static void verifyTitle(String expectedTitle) {
+        String actualTitle = Driver.get().getTitle();
+        Assert.assertEquals(expectedTitle, actualTitle);
+
+    }
+
+    //I will get the title from the user as a param and I will check
+    // my webpage contains this title
+
+    public static void verifyTitleContains(String expectedTitle) {
+        String actualTitle = Driver.get().getTitle();
+        if (actualTitle != null)
+            Assert.assertTrue(actualTitle.contains(expectedTitle));
+    }
+
+    //we will try to wain until element is visible and I will get the element as a parameter
+    public static void waitUntilElementIsVisible(WebElement element) {
+        WebDriverWait webDriverWait=new WebDriverWait(Driver.get(), Duration.ofSeconds(10));
+       webDriverWait.until(ExpectedConditions.visibilityOf(element));
+
+    }
+    //click method
+    public static void click(WebElement webElement) {
+        waitUntilElementIsVisible(webElement);
+        webElement.click();
+    }
+
+
+}
